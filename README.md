fc0eb7ec88e56deb4af3c4884a5cb3311379ca61
karmab <karimboumedhel@gmail.com>
2014-02-03 17:13:23 +0100

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,35 @@ audisp-simple
 =============
 
 auditd plugin to concatenate audit messages by serial and convert uid to usernames in messages prior to sending them to syslog
+written in python, so quite simple to  tweak
+
+Contents
+--------
+
+    README.md                          this file
+    audisp-simple.py                   the plugin
+    audisp-simple_test.py              same version, ready for testing
+    simple.conf                        configuration file for audisp, to put in /etc/audisp/plugins.d
+
+How to use it
+-------------
+
+put the plugin in the path you want, give it 750 permissions
+put the simple.conf in /etc/audisp/plugins.d
+restart auditd 
+enjoy...
+
+How does it look like
+---------------------
+Feb  3 17:07:28 k2 audisp-simple.py: serial=171972 node=k2.lab type=SYSCALL arch=x86_64 syscall=execve success=yes exit=0 a0=1e00220 a1=1e00200 a2=1e07120 a3=7fffd0624f10 items=2 ppid=18779 pid=18780 auid=root uid=root gid=root euid=root suid=root fsuid=root egid=root sgid=root fsgid=root tty=pts2 ses=7690 comm=hostname exe=/bin/hostname key=root node=k2.lab type=EXECVE argc=1 a0=/bin/hostname node=k2.lab type=CWD cwd=/root node=k2.lab type=PATH item=0 name=/bin/hostname inode=398503 dev=fd:01 mode=file,755 ouid=root ogid=root rdev=00:00 node=k2.lab type=PATH item=1 name=(null) inode=396040 dev=fd:01 mode=file,755 ouid=root ogid=root rdev=00:00 node=k2.lab type=EOE
+
+
+
+Problems?
+---------
+
+Send a mail to karimboumedhel@gmail.com or find it yourself !
+Ai seu te pego!
+
+karmab
+
---
+++ audisp-simple.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+"""
+parse audit events and sends them to syslog after changing uid to username
+"""
+
+__author__ = "Karim Boumedhel"                                                                                                                                 
+__credits__ = ["Karim Boumedhel"]                                                                                                                              
+__license__ = "GPL"                                                                                                                                            
+__version__ = "1.0"
+__maintainer__ = "Karim Boumedhel"
+__email__ = "karimboumedhel@redhat.com"
+__status__ = "Production"
+
+import os
+import signal
+import sys
+import auparse
+import syslog
+import StringIO
+
+stop = 0
+hup = 0
+au = None
+
+def term_handler(sig, msg):
+        global stop 
+        print 'received a term event'
+        stop = 1
+        sys.exit(0)
+
+def hup_handler(sig, msg):
+        global hup
+        print 'received a hup event'
+        hup = 1
+
+def reload_config():
+        global hup
+        hup = 0
+
+def none_to_null(s):
+    'used so output matches C version'
+    if s is None:
+        return '(null)'
+    else:
+        return s
+
+signal.signal(signal.SIGHUP, hup_handler)
+signal.signal(signal.SIGTERM, term_handler)
+
+#buf=sys.stdin.readlines()
+programname = os.path.basename(sys.argv[0])
+
+def coolparse(au):
+    global serial
+    global serials 
+    global output
+    result = ''
+    event_cnt = 1
+    au.reset()
+    while True:
+        if not au.first_record():
+            sys.exit(1)
+
+        record_cnt = 1
+        while True:
+            event = au.get_timestamp()
+            serial = event.serial
+            if event is None:
+                print "Error getting timestamp - aborting"
+                sys.exit(1)
+
+            au.first_field()
+            while True:
+                result += "%s=%s " % (au.get_field_name(), au.interpret_field())
+                if not au.next_field(): break
+            record_cnt += 1
+            if not au.next_record(): break
+        event_cnt += 1
+        if not serial in serials and len(serials) >0:
+                contents = output.getvalue()
+                output.close()
+                syslog.openlog(programname)
+                syslog.syslog(syslog.LOG_NOTICE, contents )
+                serials.append(serial)
+                output = StringIO.StringIO()
+                output.write("serial=%s %s" % (serial, result.replace('\n','')))
+        else:
+                if len(serials) == 0:
+                        serials.append(serial)
+                output.write(result.replace('\n',''))
+        if not au.parse_next_event(): break
+
+def main():
+        global serials 
+        global output
+        global stop
+        global hup
+        serials = []
+        output = StringIO.StringIO()
+        while stop == 0:
+                try:
+                        #buf=sys.stdin.readlines()
+                        buf=sys.stdin
+                        if hup == 1 :
+                                reload_config()
+                                continue
+                        for line in buf:
+                                au = auparse.AuParser(auparse.AUSOURCE_BUFFER, line)
+                                coolparse(au)
+                except IOError, e:
+                        continue
+                #stop = 1
+
+if  __name__ =='__main__':
+        main()
